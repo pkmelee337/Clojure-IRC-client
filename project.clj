a231b78cb552b426601750fb907dc52390680bb4
Algin Maduro <algin.maduro@student.hu.nl>
2012-06-06 00:02:18 +0200

    fixed autocompilation

--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
   :description "Example of using Google Window Builder to create Seesaw forms"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [seesaw "1.4.0"]]
-  :javac-source-path [["src/java"]]
+  :java-source-path [["src/java"]]
   :main IRC-Client.core)
